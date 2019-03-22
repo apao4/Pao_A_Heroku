4c9a8f44b188afa2ec58becfc0b70c160b8c5fdd
Alli Pao <allipao@Allis-MacBook-Pro.local>
2019-03-21 20:29:58 -0400

    Fixed the bottle pages

--- data.js
+++ data.js
@@ -5,21 +5,21 @@ const data = {
     classic: {
         name: "Classic",
         desc: "Perfect for everyone",
-        bio: "Our simplest and most classic design.  This water bottle is perfect for your every day usage or any special occasions.  Available in over 100 colours, there is definitely a Swell for you.",
+        info: "Our simplest and most classic design.  This water bottle is perfect for your every day usage or any special occasions.  Available in over 100 colours, there is definitely a Swell for you.",
         pic: "classic-pink.jpg"
     },
 
-    travler: {
+    traveler: {
         name: "Traveler",
         desc: "Perfect for the adventurist",
-        bio: "Our new design has a wider mouth that allows for easier drinking.  This bottle is perfect for those who are always looking for adventure.  Featuring a wide mouth as well as top-grade insulation, our travelors are your best adventure companion.",
+        info: "Our new design has a wider mouth that allows for easier drinking.  This bottle is perfect for those who are always looking for adventure.  Featuring a wide mouth as well as top-grade insulation, our travelors are your best adventure companion.",
         pic: "traveler-lavendar.jpg"
     },
 
     roamer: {
         name: "Roamers",
         desc: "Perfect for the wanderers",
-        bio: "This bottle is designed specifically for our big dreamers.  With a flat design, these bottles are easily travellers and allow for compact spaces.  Wherever your dreams take you, your Swell bottle will be by your side.",
+        info: "This bottle is designed specifically for our big dreamers.  With a flat design, these bottles are easily travellers and allow for compact spaces.  Wherever your dreams take you, your Swell bottle will be by your side.",
         pic: "roamer-inkwell.jpg"
     },
 };
