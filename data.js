72dff73b16225bbd800a8e3dc62a8baeb5af9355
Alli Pao <allipao@Allis-MacBook-Pro.local>
2019-03-21 22:22:41 -0400

    added all the images

--- data.js
+++ data.js
@@ -8,21 +8,31 @@ const data = {
         info: "Our simplest and most classic design.  This water bottle is perfect for your every day usage or any special occasions.  Available in over 100 colours, there is definitely a Swell for you.",
         pic1: "classic-pink.jpg",
         pic2: "classic-marble.jpg",
-        pic3: "classic-pinkmarble.jpg"
+        pic3: "classic-blue.jpg",
+        pic4: "classic-mosaic.jpg",
+        pic5: "classic-milky.jpg"
     },
 
     traveler: {
         name: "Traveler",
         desc: "Perfect for the adventurist",
         info: "Our new design has a wider mouth that allows for easier drinking.  This bottle is perfect for those who are always looking for adventure.  Featuring a wide mouth as well as top-grade insulation, our travelors are your best adventure companion.",
-        pic: "traveler-lavendar.jpg"
+        pic1: "traveler-lavendar.jpg",
+        pic2: "traveler-marble.jpg",
+        pic3: "traveler-granite.jpg",
+        pic4: "traveler-geode.jpg",
+        pic5: "traveler-onyx.jpg"
     },
 
     roamer: {
         name: "Roamers",
         desc: "Perfect for the wanderers",
         info: "This bottle is designed specifically for our big dreamers.  With a flat design, these bottles are easily travellers and allow for compact spaces.  Wherever your dreams take you, your Swell bottle will be by your side.",
-        pic: "roamer-inkwell.jpg"
+        pic1: "roamer-inkwell.jpg",
+        pic2: "roamer-fury.jpg",
+        pic3: "roamer-onyx.jpg",
+        pic4: "roamer-white.jpg",
+        pic5: "roamer-wood.jpg"
     },
 };
 
