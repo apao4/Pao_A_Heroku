0032c3fbc47e3593324e8defbd7dfc135d54bf0e
Alli Pao <allipao@Allis-MacBook-Pro.local>
2019-03-21 20:27:07 -0400

    added images and info on bottle types

--- data.js
+++ data.js
@@ -2,19 +2,26 @@ const data = {
     name: "Trevor",
     role: "confuserator",
 
-    user1: {
-        name: "user",
-        role: "developer",
-        bio: "some bio info goes here",
-        bioPic: "hannah.jpg"
+    classic: {
+        name: "Classic",
+        desc: "Perfect for everyone",
+        bio: "Our simplest and most classic design.  This water bottle is perfect for your every day usage or any special occasions.  Available in over 100 colours, there is definitely a Swell for you.",
+        pic: "classic-pink.jpg"
     },
 
-    user2: {
-        name: "user2",
-        role: "designer",
-        bio: "some bio info goes here",
-        bioPic: "hannah.jpg"
-    }
+    travler: {
+        name: "Traveler",
+        desc: "Perfect for the adventurist",
+        bio: "Our new design has a wider mouth that allows for easier drinking.  This bottle is perfect for those who are always looking for adventure.  Featuring a wide mouth as well as top-grade insulation, our travelors are your best adventure companion.",
+        pic: "traveler-lavendar.jpg"
+    },
+
+    roamer: {
+        name: "Roamers",
+        desc: "Perfect for the wanderers",
+        bio: "This bottle is designed specifically for our big dreamers.  With a flat design, these bottles are easily travellers and allow for compact spaces.  Wherever your dreams take you, your Swell bottle will be by your side.",
+        pic: "roamer-inkwell.jpg"
+    },
 };
 
 module.exports = data;
