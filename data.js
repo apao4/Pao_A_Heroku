9d9399f873a6920c119a0e915253c0e30732329e
Alli Pao <allipao@Allis-MacBook-Pro.local>
2019-03-21 20:04:28 -0400

    Initalized

---
+++ data.js
@@ -0,0 +1,22 @@
+const data = {
+    name: "Trevor",
+    role: "confuserator",
+
+    user1: {
+        name: "user",
+        role: "developer",
+        bio: "some bio info goes here",
+        bioPic: "hannah.jpg"
+    },
+
+    user2: {
+        name: "user2",
+        role: "designer",
+        bio: "some bio info goes here",
+        bioPic: "hannah.jpg"
+    }
+};
+
+module.exports = data;
+
+//throw an image here
